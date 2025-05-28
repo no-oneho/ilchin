42bf6e9023ce08d808a84aa047071e8212b89b6a
NoOneHo <a624810@naver.com>
2025-05-28 17:41:41 +0900

    init commit

---
+++ src/main/java/org/groupware/ilchin/IlchinApplication.java
@@ -0,0 +1,13 @@
+package org.groupware.ilchin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlchinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlchinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/groupware/ilchin/IlchinApplicationTests.java
@@ -0,0 +1,13 @@
+package org.groupware.ilchin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlchinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
