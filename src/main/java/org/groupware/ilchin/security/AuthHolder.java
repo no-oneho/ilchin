0435ab16e43f98bb413e8d7ad5ffd191bddb7297
NoOneHo <a624810@naver.com>
2025-05-31 17:41:41 +0900

    feat: 권한체크 어노테이션 개발

---
+++ src/main/java/org/groupware/ilchin/security/AuthHolder.java
@@ -0,0 +1,19 @@
+package org.groupware.ilchin.security;
+
+public class AuthHolder {
+
+	private static final ThreadLocal<Long> userIdxHolder = new ThreadLocal<>();
+
+	public static void setUserId(Long userId) {
+		userIdxHolder.set(userId);
+	}
+
+	public static Long getUserId() {
+		return userIdxHolder.get();
+	}
+
+	public static void clearUserIdx() {
+		userIdxHolder.remove();
+	}
+
+}
