e02c685df655e9bed7ac0a913e97da664e95e1f1
NoOneHo <a624810@naver.com>
2025-06-11 18:52:02 +0900

    feat: spring valid 적용

--- src/main/java/org/groupware/ilchin/dto/department/request/CreateReq.java
+++ src/main/java/org/groupware/ilchin/dto/department/request/CreateReq.java
@@ -1,9 +1,11 @@
 package org.groupware.ilchin.dto.department.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateReq(
-        String name,
-        String description,
-        Long managerUserId,
+        @NotBlank(message = "부서명은 빈 값일 수 없습니다.") String name,
+        @NotBlank(message = "부서설명은 빈 값일 수 없습니다.") String description,
+        @NotBlank(message = "부서장을 선택해주세요.") Long managerUserId,
         String tel
 ) {
 }
